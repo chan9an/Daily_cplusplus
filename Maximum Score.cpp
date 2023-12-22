017cc2ce29ed5ea7c06843cab0174a4d49ebee23
chan9an <141244019+chan9an@users.noreply.github.com>
2023-12-22 21:20:25 +0530

    Create Maximum Score.cpp

---
+++ Maximum Score.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    
+	    int temp;
+	    int onecount = 0;
+	    int zerocount = 0;
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> temp;
+	        
+	        if(temp == 1){
+	            onecount++;
+	        }
+	        else{
+	            zerocount++;
+	        }
+	    }
+	    
+	    cout << min(onecount, zerocount) << endl;
+	}
+	return 0;
+}
