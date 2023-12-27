c22f628947b21141ed8dcc47fd956b5e5b2caac5
chan9an <141244019+chan9an@users.noreply.github.com>
2023-12-27 21:55:09 +0530

    Create Painting Walls.cpp

---
+++ Painting Walls.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y,z,a,p;
+	cin>>t;
+	for(int i=0;i<t;i++){
+	    cin>>x>>y>>z;
+	   a=x*y;
+	   p=z/2;
+	   cout<<p/a;
+	    
+	}
+	return 0;
+
+}
