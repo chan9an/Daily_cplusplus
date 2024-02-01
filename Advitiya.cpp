dd89bf05df013fbccd8ff0d4076bbed1688a48f1
chan9an <141244019+chan9an@users.noreply.github.com>
2024-02-01 23:10:46 +0530

    Create Advitiya.cpp

---
+++ Advitiya.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+   int N;
+   cin>>N;
+   if(N==16||N==17||N==18)
+   {
+       cout<<"ADVITIYA"<<endl;
+   }
+   else
+   {
+       cout<<"WAITING FOR ADVITIYA"<<endl;
+   }
+   return 0;
+}
