f4a8b8764922e710f71626884a9bf991eebd7cd1
chan9an <141244019+chan9an@users.noreply.github.com>
2024-02-12 23:47:06 +0530

    Create Valentines Contest.cpp

---
+++ Valentines Contest.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pll pair<long long ,long long > 
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n;
+    cin>>n;
+    if(n==121) cout<<"Likely"<<endl;
+    else cout<<"Unlikely"<<endl;
+	return 0;
+}
