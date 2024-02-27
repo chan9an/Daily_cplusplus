180468bc707b63f6c2bd6190ccdb2f1173ffc63a
chan9an <141244019+chan9an@users.noreply.github.com>
2024-02-27 23:40:16 +0530

    Create Healthy Sleep.cpp

---
+++ Healthy Sleep.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+   int h;cin>>h;
+   if(h<8) cout<<"LESS"<<endl;
+ else   if(h>8) cout<<"MORE"<<endl;
+else if(h==8) cout<<"PERFECT"<<endl;
+
+    
+}
