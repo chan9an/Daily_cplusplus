4924dc4a52e2d6e9798d7a043a3f260926463573
chan9an <141244019+chan9an@users.noreply.github.com>
2024-03-04 23:23:00 +0530

    Create Minimum And Maximum II.cpp

---
+++ Minimum And Maximum II.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    long long n;
+    cin>>n;
+    cout<<(n*(n+1)/2 +n -1)<<endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)solve();
+    return 0;
+}
