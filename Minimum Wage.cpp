c9aa77e31425ea52a073f3b1f5bbcde8848d801e
chan9an <141244019+chan9an@users.noreply.github.com>
2024-03-13 23:59:33 +0530

    Create Minimum Wage.cpp

---
+++ Minimum Wage.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x;
+	cin>>x;
+	if(x>11)
+	cout<<"yes"<<endl;
+	else
+	cout<<"no"<<endl;
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
