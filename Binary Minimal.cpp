c85182fc1cce10a02e7524210f1c3ca4a2dd4065
chan9an <141244019+chan9an@users.noreply.github.com>
2024-03-17 23:19:07 +0530

    Create Binary Minimal.cpp

---
+++ Binary Minimal.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n, k;
+	    cin >> n >> k;
+	    string s;
+	    cin >> s;
+	    vector<int> ind;
+	    int cnt = 0;
+	    for(int i =0; i<n; i++){
+	        if(s[i] == '1')
+	        {
+	            cnt++;
+	        }
+	    }
+	    if(cnt > k){
+	        for(int i=0; i<n; i++)
+	        {
+	            if(s[i] == '1' && k>0)
+	            {
+	                s[i] ='0';
+	                k--;
+	            }
+	        }
+	        cout << s << endl;
+	    }
+	    else
+	    {
+	        for(int i=0; i <(n-k); i++)
+	        {
+	            cout << '0';
+	        }
+	        cout << endl;
+	    }
+	}
+	return 0;
+
+}
