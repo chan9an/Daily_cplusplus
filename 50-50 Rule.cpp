ba84fb6a883399b755e9dbc1c60f2616242c1faa
chan9an <141244019+chan9an@users.noreply.github.com>
2024-03-19 22:39:43 +0530

    Create 50-50 Rule.cpp

---
+++ 50-50 Rule.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>> t;
+    while(t--){
+    int x,y;
+    cin>>x>>y;
+
+           if (x < 50) {
+            cout << "Z" << endl;
+        } else if (y < 50) {
+            cout << "F" << endl;
+        } else {
+            cout << "A" << endl;
+        }
+ 
+}
+}
+	// your code goes here
