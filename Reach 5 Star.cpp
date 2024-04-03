084374b427bb59941d82b6253d0a294e70eea864
chan9an <141244019+chan9an@users.noreply.github.com>
2024-04-03 23:35:48 +0530

    Create Reach 5 Star.cpp

---
+++ Reach 5 Star.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x , y;
+	cin>>x>>y;
+	x = x + y;
+	if(x>= 2000){
+	    cout<<"YES"<<endl;
+	    
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+return 0;
+}
