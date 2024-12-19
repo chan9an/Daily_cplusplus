d4672ff57f2c75ae4982cb892dffd2273112a39f
Chandan <141244019+chan9an@users.noreply.github.com>
2024-12-20 00:13:14 +0530

    Create firstquestionatstop.cpp

---
+++ firstquestionatstop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cout<<"wntee";
+	cin>>n;
+	while(n!=42){
+		cout<<n<<endl;
+		cin>>n;
+	}
+	return 0;
+}
